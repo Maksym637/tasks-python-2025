7fda5bf3caa37bd865a3bfa44305e6fb5fb9f920
moliinyk <maks.oliynik228@gmail.com>
2026-05-17 14:30:20 +0200

    CodeSignal: Projection, Filtering, and Aggregation of Data Streams - part 1

---
+++ coding_topics/projection_filtering_aggregation/data_projection_class_example.py
@@ -0,0 +1,22 @@
+# Defining our class
+class DataProjector:
+    def __init__(self, data):
+        self.data = data
+
+    def project(self, func):
+        return list(map(func, self.data))
+
+    def filter_and_project(self, filter_func, project_func):
+        filtered_data = filter(filter_func, self.data)
+        return list(map(project_func, filtered_data))
+
+
+if __name__ == "__main__":
+    sentences = ["HELLO WORLD", "PYTHON IS FUN", "I LIKE PROGRAMMING"]
+    projector = DataProjector(sentences)
+
+    lower_filtered_sentences_list = projector.filter_and_project(
+        lambda sentence: "PYTHON" in sentence,
+        lambda sentence: sentence.lower(),
+    )
+    print(lower_filtered_sentences_list)
